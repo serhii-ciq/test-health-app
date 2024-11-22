440ed184a6c7c9ee70df121f8a8184f222409396
Serhii Vasyliev <serhii.vasyliev@creatoriq.com>
2024-11-22 18:16:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
 import boto3
 
-# Replace these with your actual AWS credentials
-ACCESS_KEY = "AKIASLQ33UGGZDGIFMXW"
-SECRET_KEY = "vUjnzP8Vutc6Pr/kxwECEW87W9/lbvRmnhYJv5IK"
 
-def list_s3_buckets(access_key, secret_key):
-    # Create an S3 client using the provided access and secret keys
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key
-    )
-    
-    # Retrieve and print the list of S3 buckets
-    response = s3_client.list_buckets()
-    buckets = response.get("Buckets", [])
-    
-    print("S3 Buckets:")
-    for bucket in buckets:
-        print(f"  - {bucket['Name']}")
-
-# Call the function
-list_s3_buckets(ACCESS_KEY, SECRET_KEY)
